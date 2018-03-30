939f040ac14a214f74f0ac41a8e4bbf7db856d3f
Gaven Neufeld <gavensmac@Gavens-MacBook-Pro.local>
2018-03-30 14:06:08 -0600

    Added Style to header, added custom values, and added subheading

--- src/components/app.js
+++ src/components/app.js
@@ -3,9 +3,17 @@ import React, { Component } from 'react';
 export default class App extends Component {
   render() {
     return (
-      <div>
-        <h1>Letters to Santa</h1>
-      </div>
+        <div>
+          <div className="background">
+            <div className="letters-heading">
+              <h1>Letters to Santa</h1>
+              <div className="letters-subheading">
+                Santa wants to know what you want for this year! <br/>Fill out your list and click the submit button to send your list to the North Pole!
+              </div>
+           </div>
+
+        </div>  
+      </div>   
     );
   }
 }
